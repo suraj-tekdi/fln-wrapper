3c64b09d31e828f46a2810b75218f7fb46fc1184
Yash Mittal <mittal.yash12@hotmail.com>
2022-11-30 11:05:32 +0530

    Feat: working search with filters
    Signed-off-by: Yash Mittal <mittal.yash12@hotmail.com>

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,4 +1,4 @@
-import { Controller, Get } from '@nestjs/common';
+import { Body, Controller, Get, Post } from '@nestjs/common';
 import { AppService } from './app.service';
 
 @Controller()
@@ -10,8 +10,8 @@ export class AppController {
     return this.appService.getHello();
   }
 
-  @Get('courses')
-  getCoursesFromSwayam() {
-    return this.appService.getCoursesFromSwayam();
+  @Post('courses')
+  getCoursesFromSwayam(@Body() body: any) {
+    return this.appService.getCoursesFromSwayam(body);
   }
 }
--- src/app.service.ts
+++ src/app.service.ts
@@ -9,73 +9,83 @@ export class AppService {
     return 'Hello World!';
   }
 
-  async getCoursesFromSwayam() {
+  async getCoursesFromSwayam(body: any) {
+    const {
+      provider,
+      courseMode,
+      courseDuration,
+      courseCredits,
+      courseCategory,
+      query,
+    } = body;
+    const gql = `{
+      courseList(
+        args: {
+          includeClosed: false
+          filterText: ${query ? query : '""'}
+          category: ${courseCategory ? courseCategory : '""'}
+          status: "Upcoming"
+          tags: ""
+          duration: ${courseDuration ? courseDuration : '"all"'}
+          examDate: "all"
+          credits: ${courseCredits ? courseCredits : '"all"'}
+          ncCode: ${provider ? provider : '"all"'}
+          courseType: ${courseMode ? courseMode : '"all"'}
+        }
+        first: 100
+      ) {
+        edges {
+          node {
+            id
+            title
+            url
+            explorerSummary
+            explorerInstructorName
+            enrollment {
+              enrolled
+            }
+            openForRegistration
+            showInExplorer
+            startDate
+            endDate
+            examDate
+            enrollmentEndDate
+            estimatedWorkload
+            category {
+              name
+              category
+              parentId
+            }
+            tags {
+              name
+            }
+            featured
+            coursePictureUrl
+            credits
+            weeks
+            nodeCode
+            instructorInstitute
+            ncCode
+            semester
+          }
+        }
+        pageInfo {
+          endCursor
+          hasNextPage
+        }
+      }
+      examDates {
+        date
+      }
+    }
+    `;
+    console.log('gql', gql);
     try {
       const resp = await lastValueFrom(
         this.httpService
           .get('https://swayam.gov.in/modules/gql/query', {
             params: {
-              q: `{
-                courseList(
-                  args: {
-                    includeClosed: false
-                    filterText: ""
-                    category: "Humanities_and_Arts,Anthropology,Arts,Communications,Economics,English,Geography,History,Humanities_and_Social_Sciences,Journalism_(mass_media),Language,Linguistics,Philosophy,Political_Science,Sociology"
-                    status: "Upcoming"
-                    tags: ""
-                    duration: "all"
-                    examDate: "all"
-                    credits: "all"
-                    ncCode: "all"
-                    courseType: "all"
-                  }
-                  first: 100
-                ) {
-                  edges {
-                    node {
-                      id
-                      title
-                      url
-                      explorerSummary
-                      explorerInstructorName
-                      enrollment {
-                        enrolled
-                      }
-                      openForRegistration
-                      showInExplorer
-                      startDate
-                      endDate
-                      examDate
-                      enrollmentEndDate
-                      estimatedWorkload
-                      category {
-                        name
-                        category
-                        parentId
-                      }
-                      tags {
-                        name
-                      }
-                      featured
-                      coursePictureUrl
-                      credits
-                      weeks
-                      nodeCode
-                      instructorInstitute
-                      ncCode
-                      semester
-                    }
-                  }
-                  pageInfo {
-                    endCursor
-                    hasNextPage
-                  }
-                }
-                examDates {
-                  date
-                }
-              }
-              `,
+              q: gql,
               expanded_gcb_tags: 'gcb-markdown',
             },
           })

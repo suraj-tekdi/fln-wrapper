86ac3caa46f5deafa76d2499f4ebbe364baf71b0
Suraj <suraj_k@tekditechnologies.com>
2023-07-15 09:48:41 +0530

    fln-wrapper

--- src/main.ts
+++ src/main.ts
@@ -3,7 +3,7 @@ import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  await app.listen(process.env.PORT || 6000);
-  console.log(`Server started on port ${process.env.PORT || 6000}`);
+  await app.listen(process.env.PORT || 6002);
+  console.log(`Server started on port ${process.env.PORT || 6002}`);
 }
 bootstrap();
